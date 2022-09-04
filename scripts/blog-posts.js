2186e2c3ca7e75e7de1c96383ba6bf3532e1d5b6
Nilambar Sharma <nilambar@outlook.com>
2022-09-04 16:36:52 +0545

    feat: new welcome page

---
+++ scripts/blog-posts.js
@@ -0,0 +1,58 @@
+( function( $ ) {
+	$.fn.blogPosts = function( options ) {
+		var settings = $.extend( {
+			api: '',
+			action: 'blog_posts',
+			loading_text: 'Loading',
+			list_type: 'ul',
+		}, options );
+
+		if ( '' === settings.api ) {
+			return this;
+		}
+
+		function generateList( data ) {
+			var output = '';
+
+			if ( 0 === data.length ) {
+				return output;
+			}
+
+			data.forEach( function( item ) {
+				output += '<li><a href="' + item.url + '" target="_blank">' + item.title + '</a></li>';
+			} );
+
+			return $( '<' + settings.list_type + '/>' ).append( output );
+		}
+
+		return this.each( function() {
+			var $wrapper = $( this );
+
+			$.ajax( {
+				url: settings.api,
+				type: 'GET',
+				dataType: 'json',
+				data: { action: settings.action },
+				beforeSend: function() {
+					$wrapper.html( settings.loading_text );
+				},
+				complete: function( jqXHR ) {
+					var response = JSON.parse( jqXHR.responseText );
+
+					$wrapper.html( '' );
+
+					if ( true === response.success ) {
+						var listMarkup = generateList( response.data );
+						$wrapper.append( listMarkup );
+					}
+				},
+			} );
+		} );
+	};
+
+	$( '.ns-blog-list' ).blogPosts( {
+		api: ajaxurl,
+		action: 'nsbl_get_posts',
+	} );
+}( jQuery ) );
+
